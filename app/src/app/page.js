a4caef1ba0f8190c280b30d35560ce3616a89fa4
Jose Guadalupe Coca <becario@advanpro.com.mx>
2025-01-20 15:09:19 -0600

    Primir diseño login

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,17 +1,11 @@
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
-import ModulesSection from '@/components/ModulesSection';
+import LoginForm from "@/components/LoginForm";
+import LoginCarousel from "@/components/LoginCarousel";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 ml-20">
-        <Navbar />
-        <main className="min-h-screen bg-gradient-to-br from-[#f0f9ff] via-[#f8fcff] to-[#edf8ff]">
-          <ModulesSection />
-        </main>
-      </div>
-    </div>
+    <main className="flex min-h-screen">
+      <LoginCarousel />
+      <LoginForm />
+    </main>
   );
 }
